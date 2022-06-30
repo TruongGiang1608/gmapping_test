c12ba34ff4f85c42bd8d0bcb66882586a598b6e0
TruongGiang1608 <100744043+TruongGiang1608@users.noreply.github.com>
2022-06-30 11:19:03 +0700

    Update scan_matcher_vasco.cpp

--- scan_matcher_vasco.cpp
+++ scan_matcher_vasco.cpp
@@ -4,7 +4,9 @@
 #include <vector>
 #include <tf/transform_broadcaster.h>
 #include <tf2_ros/transform_broadcaster.h>
-
+#include <geometry_msgs/PoseArray.h>
+#include <random>
+#include <geometry_msgs/PoseStamped.h>
 using namespace std;
 
 // struct pose{
@@ -23,7 +25,7 @@ void odomCallback(const nav_msgs::Odometry& msg) {
 	odom_s[1] = msg.pose.pose.position.y;
 	odom_s[2] = tf::getYaw(msg.pose.pose.orientation);
 }
-const int M = 100;
+const int M = 1;
 void SampleInitial(double (&initial_pose)[3][M]) {
 	random_device s0;
 	mt19937 rng0(s0());
@@ -34,19 +36,19 @@ void SampleInitial(double (&initial_pose)[3][M]) {
 		}
 	}
 }
-void poseGuess(double odom_pose_suc[3], double odom_pose_pre[3], double pose_est_pre[3][M], double (&pose_g)[3][M]) {
+void poseGuess(double odom_pose_suc[3], double odom_pose_pre[3], double pose_est_pre[3][M], double (&pose_t)[3][M]) {
     for (int i = 0; i < M; i++) {
-        pose_g[0][i] = pose_est_pre[0][i] + odom_pose_suc[0] - odom_pose_pre[0];
-        pose_g[1][i] = pose_est_pre[1][i] + odom_pose_suc[1] - odom_pose_pre[1];
-        pose_g[2][i] = pose_est_pre[2][i] + odom_pose_suc[2] - odom_pose_pre[2];
+        pose_t[0][i] = pose_est_pre[0][i] + odom_pose_suc[0] - odom_pose_pre[0];
+        pose_t[1][i] = pose_est_pre[1][i] + odom_pose_suc[1] - odom_pose_pre[1];
+        pose_t[2][i] = pose_est_pre[2][i] + odom_pose_suc[2] - odom_pose_pre[2];
     }
 }
 
 int main (int argc, char **argv) {
     ros::init(argc, argv, "scan_matcher_vasco");
     ros::NodeHandle nh;
-    ros::Publisher pose_guess_pub = nh.advertise<std_msgs::Float64MultiArray>("pose_guess", 1000);
-    ros::Subscriber odom_sub = = nh.subscribe("odom", 1000, odomCallback);
+    ros::Publisher pose_guess_pub = nh.advertise<geometry_msgs::PoseStamped>("pose_guess", 1000);
+    ros::Subscriber odom_sub = nh.subscribe("odom", 1000, odomCallback);
 
     tf2_ros::TransformBroadcaster pose_broadcaster;
     ROS_INFO("Publishing pose samples over ROS");
@@ -57,18 +59,34 @@ int main (int argc, char **argv) {
     while(ros::ok()) {
         ros::spinOnce();
         poseGuess(odom_s, odom_t, pose_t, pose_t);
+        //for(int i = 0; i < M; i++) {
+            geometry_msgs::Quaternion pose_quat = tf::createQuaternionMsgFromYaw(pose_t[2][0]);
 
-        geometry_msgs::TransformStamped pose_trans;
-        pose_trans.header.stamp = current_time;
-        pose_trans.header.frame_id = "map";
-        pose_trans.child_frame_id = "base_footprint";
-  
-        pose_trans.transform.translation.x = pose_g[0][i];
-        pose_trans.transform.translation.y = pose_g[1][i];
-        pose_trans.transform.translation.z = 0.0;
-        pose_trans.transform.rotation = pose_quat;
-  
-        pose_broadcaster.sendTransform(pose_trans);
+            geometry_msgs::TransformStamped pose_trans;
+            pose_trans.header.stamp = ros::Time::now();
+            pose_trans.header.frame_id = "map";
+            pose_trans.child_frame_id = "base_footprint";
+    
+            pose_trans.transform.translation.x = pose_t[0][0];
+            pose_trans.transform.translation.y = pose_t[1][0];
+            pose_trans.transform.translation.z = 0.0;
+            pose_trans.transform.rotation = pose_quat;
+    
+            pose_broadcaster.sendTransform(pose_trans);
+
+            geometry_msgs::PoseStamped pose_guess;
+            pose_guess.header.stamp = ros::Time::now();
+            pose_guess.header.frame_id = "map";
+
+            geometry_msgs::Pose p;
+            pose_guess.pose.position.x = pose_t[0][0];
+            pose_guess.pose.position.y = pose_t[1][0];
+            pose_guess.pose.position.z = 0.0;
+            pose_guess.pose.orientation = pose_quat;
+        //}
+        pose_guess_pub.publish(pose_guess);
+        rate.sleep();
+        
     }
 
 }
